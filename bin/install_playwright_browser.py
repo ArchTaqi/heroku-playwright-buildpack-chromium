3e6ad0f649b35220f18c66c71680addd5ad09902
gokhantuffer <65483960+gokhantuffer@users.noreply.github.com>
2022-07-19 14:35:10 +0300

    Update install_playwright_browser.py

--- bin/install_playwright_browser.py
+++ bin/install_playwright_browser.py
@@ -1,4 +1,4 @@
 import os, sys
-python_version = sys.version_info.major + "." + sys.version_info.minor
+python_version = str(sys.version_info.major) + "." + str(sys.version_info.minor)
 os.environ["PLAYWRIGHT_BROWSERS_PATH"]="/app/.heroku/python/lib/python" + python_version + "/site-packages/playwright/driver/package/.local-browsers/"
 os.system("python -m playwright install chromium")
